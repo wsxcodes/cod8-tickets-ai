5e01e8b05eb8f504188d48fb7f7c900fd44017a3
Jan Filips <janfilipsgt@gmail.com>
2025-03-06 14:52:09 +0200

    feat: add workers endpoint for processing and uploading tickets

---
+++ backend/api/api_v1/endpoints/workers_endpoints.py
@@ -0,0 +1,20 @@
+import logging
+
+from fastapi import APIRouter
+
+from backend.decorators import log_endpoint
+from workers.process_tickets_worker import (process_ticket_csv, upload_ticket,
+                                            vectorize_ticket)
+
+logger = logging.getLogger(__name__)
+
+router = APIRouter()
+
+
+@router.get("/process_tickets")
+@log_endpoint
+def process_tickets(csv_filename: str = "data/Bank_Design_Equipment_FY2024.csv"):
+    tickets_df = process_ticket_csv(csv_filename)
+    for _, ticket in tickets_df.iterrows():
+        ticket = vectorize_ticket(ticket)
+        upload_ticket(ticket)
