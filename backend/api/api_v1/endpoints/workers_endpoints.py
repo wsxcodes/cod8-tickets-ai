4d23b917b7748ecf23368b1453f0f0b5579a3dc3
Jan Filips <janfilipsgt@gmail.com>
2025-03-09 01:01:16 +0200

    fix: rename endpoint for importing historical tickets and update function definition

--- backend/api/api_v1/endpoints/workers_endpoints.py
+++ backend/api/api_v1/endpoints/workers_endpoints.py
@@ -2,8 +2,6 @@ import logging
 
 from fastapi import APIRouter
 
-from workers.process_tickets_worker import (process_ticket_csv, upload_ticket,
-                                            vectorize_ticket)
 
 logger = logging.getLogger(__name__)
 
@@ -11,12 +9,12 @@ router = APIRouter()
 
 
 # XXX TODO add customer (create DB schema per customer etc etc..)
-@router.get("/generate_vector_knowledgebase")
-def process_historical_tickets(csv_filename: str = "data/Bank_Design_Equipment_FY2024.csv"):
+@router.get("/import_historical_tickets")
+def import_historical_tickets(csv_filename: str = "data/Bank_Design_Equipment_FY2024.csv"):
     """
     Generate a vectorized knowledgebase from historical support tickets.
     """
-    tickets_df = process_ticket_csv(csv_filename)
+    tickets_df = import_ticket_csv(csv_filename)
     for _, ticket in tickets_df.iterrows():
         ticket = vectorize_ticket(ticket)
         upload_ticket(ticket)
