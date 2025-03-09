ae97f3c828881a494b7e67e6196e87104d0a7a47
Jan Filips <janfilipsgt@gmail.com>
2025-03-09 15:30:47 +0200

    fix: add blank line for better readability in workers_endpoints.py

--- backend/api/api_v1/endpoints/workers_endpoints.py
+++ backend/api/api_v1/endpoints/workers_endpoints.py
@@ -8,6 +8,7 @@ router = APIRouter()
 
 
 # XXX TODO add customer (create DB schema per customer etc etc..)
+
 @router.get("/import_historical_tickets")
 async def import_historical_tickets(csv_filename: str = "data/Bank_Design_Equipment_FY2024.csv"):
     """
