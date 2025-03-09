3bd973494dadec39aebf98a12a50419a7ee59629
Jan Filips <janfilipsgt@gmail.com>
2025-03-09 15:04:23 +0200

    fix: change import_historical_tickets function to be asynchronous

--- backend/api/api_v1/endpoints/workers_endpoints.py
+++ backend/api/api_v1/endpoints/workers_endpoints.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import APIRouter
+from workers.process_tickets_worker import 
 
 logger = logging.getLogger(__name__)
 
@@ -9,11 +10,9 @@ router = APIRouter()
 
 # XXX TODO add customer (create DB schema per customer etc etc..)
 @router.get("/import_historical_tickets")
-def import_historical_tickets(csv_filename: str = "data/Bank_Design_Equipment_FY2024.csv"):
+async def import_historical_tickets(csv_filename: str = "data/Bank_Design_Equipment_FY2024.csv"):
     """
     Generate a vectorized knowledgebase from historical support tickets.
     """
-    tickets_df = import_ticket_csv(csv_filename)
-    for _, ticket in tickets_df.iterrows():
-        ticket = vectorize_ticket(ticket)
-        upload_ticket(ticket)
+    # XXX TODO
+    ...
