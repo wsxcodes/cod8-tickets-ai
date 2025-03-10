74677a7df4b4d1cc6beb34100088e22d3be56a65
Jan Filips <janfilipsgt@gmail.com>
2025-03-10 20:07:03 +0200

    feat: add backend dependencies for chat history management and OpenAI integration

---
+++ backend/dependencies.py
@@ -0,0 +1,28 @@
+import logging
+
+import semantic_kernel as sk
+from fastapi import APIRouter, HTTPException
+from openai import OpenAI
+from pydantic import BaseModel
+from semantic_kernel.connectors.ai.function_choice_behavior import \
+    FunctionChoiceBehavior
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.azure_chat_prompt_execution_settings import \
+    AzureChatPromptExecutionSettings
+from semantic_kernel.contents.chat_history import ChatHistory
+
+from backend import config
+
+logger = logging.getLogger(__name__)
+
+session_histories = {}
+
+def get_history(session_id: str) -> ChatHistory:
+    if session_id not in session_histories:
+        session_histories[session_id] = ChatHistory()
+    return session_histories[session_id]
+
+kernel = sk.Kernel()
+execution_settings = AzureChatPromptExecutionSettings()
+execution_settings.function_choice_behavior = FunctionChoiceBehavior.Auto()
+openai_client = OpenAI(api_key=config.CHATGPT_KEY)
