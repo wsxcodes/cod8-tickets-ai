ffb8d9dbf083a2c50fa3e66997b02bb375deaaff
Jan Filips <janfilipsgt@gmail.com>
2025-03-10 21:48:37 +0200

    feat: add logging to import historical tickets endpoint

--- backend/api/api_v1/endpoints/workers_endpoints.py
+++ backend/api/api_v1/endpoints/workers_endpoints.py
@@ -10,6 +10,7 @@ router = APIRouter()
 # XXX TODO add customer (create DB schema per customer etc etc..)
 
 @router.get("/import_historical_tickets")
+@log_endpoint
 async def import_historical_tickets(csv_filename: str = "data/Bank_Design_Equipment_FY2024.csv"):
     """
     Generate a vectorized knowledgebase from historical support tickets.
