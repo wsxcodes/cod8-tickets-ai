837f23d09c2aa3435dd8ba20800fe5986b4b776c
Jan Filips <janfilipsgt@gmail.com>
2025-03-18 13:24:19 +0200

    feat: reorganize imports and add newlines for improved readability in utils.py

--- backend/helpers/utils.py
+++ backend/helpers/utils.py
@@ -1,8 +1,10 @@
 import logging
 import smtplib
 from email.mime.text import MIMEText
-from backend import config
+
 from semantic_kernel.utils.logging import setup_logging
+
+from backend import config
 
 # Set up logging for the kernel
 setup_logging()
@@ -26,11 +28,11 @@ def send_email(to_addr: str, subject: str, body: str):
     msg['To'] = to_addr
 
     logging.info("Attempting to send email from %s to %s with subject '%s'", from_addr, to_addr, subject)
-    
+
     # Connect, log in, send
     with smtplib.SMTP(smtp_host, smtp_port) as server:
         server.starttls()  # Remove if using port 465 + SMTP_SSL
         server.login(username, password)
         server.send_message(msg)
-    
+
     logging.info("Email successfully sent to %s", to_addr)
