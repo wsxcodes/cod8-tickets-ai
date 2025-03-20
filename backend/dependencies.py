10a1760af943ca09f15f8130bffd20e24d9e7e49
Jan Filips <janfilipsgt@gmail.com>
2025-03-20 19:43:09 +0200

    fix: update parameter name for AzureChatCompletion deployment

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -18,7 +18,7 @@ execution_settings.function_choice_behavior = FunctionChoiceBehavior.Auto()
 openai_client = OpenAI(api_key=config.CHATGPT_KEY)
 
 chat_completion = AzureChatCompletion(
-    AZURE_OPENAI_DEPLOYMENT_NAME=config.AZURE_OPENAI_DEPLOYMENT_NAME,
+    deployment_name=config.AZURE_OPENAI_DEPLOYMENT_NAME,
     endpoint=config.OPENAI_ENDPOINT,
     api_key=config.OPENAI_API_KEY,
     api_version=config.AZURE_OPENAI_MODEL_VERSION
